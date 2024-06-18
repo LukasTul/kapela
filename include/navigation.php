947b41f68d0dfac42a508a566c5e29f43a43260d
Lukas Kunt <lukas.kunt@hardwario.com>
2024-06-18 10:35:37 +0200

    navagation shringed while singed in

--- include/navigation.php
+++ include/navigation.php
@@ -10,9 +10,7 @@
     if (isset($_SESSION['user_id'])) {
         $userId = $_SESSION['user_id'];
         // Now you can use $userId
-    } else {
-        echo("User ID is not set in the session.");
-    }
+    } 
 ?>
 <body id="page-top">
     <!-- Navigation-->
@@ -30,26 +28,28 @@
                     <li class="nav-item"><a class="nav-link" href="#diskografie">Diskografie</a></li>
                     <li class="nav-item"><a class="nav-link" href="#poradatele">Pro pořadatele</a></li> -->
                     <li class="nav-item"><a class="nav-link" href="#kontakt">Kontakt</a></li>
-                    <li class="nav-item"><a class="nav-link" href="userRegisterForm.php">Registrace</a></li>
+                   
                     <?php
                     if (isset($_SESSION['user_nickname'])) {
                         $navKomentsString = '<li class="nav-item"><a class="nav-link" href="komentForm.php">Nový komentář</a></li>
                                            <li class="nav-item"><a class="nav-link" href="komentsView.php">Zobrazení komentářů</a></li>
-                                           <li class="nav-item"><a class="nav-link" href="komentsEdit.php">Editace komentářů</a></li>';
+                                           <li class="nav-item"><a class="nav-link" href="komentsEdit.php">Editace komentářů</a></li>
+                                           <li class="nav-item"><a class="nav-link" href="userLogout.php">Odhlášení</a></li>';
                         echo $navKomentsString;
                     }
+                    else {
+                        echo '<li class="nav-item"><a class="nav-link" href="userLoginForm.php">Přihlášení</a></li>
+                         <li class="nav-item"><a class="nav-link" href="userRegisterForm.php">Registrace</a></li>';
+                    }
                     if (isset($_SESSION['user_admin']) && $_SESSION['user_admin'] == 1) {
-                        echo '<li class="nav-item"><a class="nav-link" href="usersEdit.php">Editace profilů</a></li>';¨
+                        echo '<li class="nav-item"><a class="nav-link" href="usersEdit.php">Editace profilů</a></li>';
                     } 
-                    
                     else {
                         if (isset($_SESSION['user_nickname'])) {
                             echo '<li class="nav-item"><a class="nav-link" href="usersEditItem.php?user_id=' . $_SESSION['user_id'] . '">Editace profilu</a></li>';
                         }
                     }
                     ?>
-                    <li class="nav-item"><a class="nav-link" href="userLoginForm.php">Přihlášení</a></li>
-                    <li class="nav-item"><a class="nav-link" href="userLogout.php">Odhlášení</a></li>
                     <li class="nav-item"><span class="nav-link text-<?php echo isset($_SESSION['user_nickname']) ? 'success' : 'danger'; ?>"><?php echo $user_display; ?></span></li>
                 </ul>
             </div>
